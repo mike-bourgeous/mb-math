0603879d406738bf63c5a8fab2350e213a67f36e
Mike Bourgeous <mike@mikebourgeous.com>
2021-07-09 16:19:31 -0600

    Support arrays and enumerators in #safe_power.

--- lib/mb/m/exponential_methods.rb
+++ lib/mb/m/exponential_methods.rb
@@ -5,7 +5,7 @@ module MB
       # Raises the given +value+ to the given +power+, but using the absolute
       # value function to prevent complex results.  Useful for waveshaping.
       def safe_power(value, power)
-        if value.is_a?(Numo::NArray)
+        if !value.is_a?(Numeric) && value.respond_to?(:map)
           return value.map { |v| safe_power(v, power) }
         end
 
