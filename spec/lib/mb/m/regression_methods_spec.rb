2776f9c0148cdd2573039f338d92e29d34d62f10
Mike Bourgeous <mike@mikebourgeous.com>
2024-06-19 16:49:34 -0600

    Added a simple test of linear regression with noisy data.

--- spec/lib/mb/m/regression_methods_spec.rb
+++ spec/lib/mb/m/regression_methods_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe(MB::M::RegressionMethods) do
+RSpec.describe(MB::M::RegressionMethods, aggregate_failures: true) do
   context 'with perfect data' do
     it 'returns the correct slope and intercept for y=x' do
       expect(MB::M.linear_regression(Numo::SFloat[0, 1, 2, 3, 4, 5])).to eq([1, 0])
@@ -30,6 +30,16 @@ RSpec.describe(MB::M::RegressionMethods) do
   end
 
   context 'with noisy data' do
-    pending 'TODO'
+    it 'returns expected coefficients for y=-0.125x+75.5 with noise at 0.01' do
+      data = Numo::DFloat.linspace(75.5, -24.375, 800)
+      m, b = MB::M.linear_regression(data)
+      expect(m).to be_within(0.00000001).of(-0.125)
+      expect(b).to be_within(0.00000001).of(75.5)
+
+      noise = Numo::DFloat.zeros(800).map { rand() * 0.02 - 0.01 }
+      m, b = MB::M.linear_regression(data + noise)
+      expect(m).to be_within(0.002).of(-0.125)
+      expect(b).to be_within(0.002).of(75.5)
+    end
   end
 end
