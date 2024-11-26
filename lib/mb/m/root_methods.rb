ccec69dbea9083e9b69867fcbb6266445bc9bcdb
Mike Bourgeous <mike@mikebourgeous.com>
2024-11-26 11:42:09 -0700

    Revert "Try restoring some more code to get root grid working again."
    This reverts commit f266e3015de7034296af68bc4207b497749a38c9.

--- lib/mb/m/root_methods.rb
+++ lib/mb/m/root_methods.rb
@@ -247,7 +247,7 @@ module MB
 
           # Possible multiple root; try finding root of f(x)/f'(x) instead of f(x)
           yprime = f_prime.call(x)
-          if yprime.abs < tolerance ** 2 && y != 0 && (step.nil? || step.abs > tolerance) && depth < 2
+          if yprime.abs < tolerance ** 2 && y != 0 && (step.nil? || step.abs > tolerance ** 2) && depth < 2
             MB::U.headline("#{prefix}\e[1;34mTrying multiple root method at depth=#{depth}\e[0m", prefix: prefix) if $DEBUG
 
             new_x, new_y = multi_root(x, f: f, f_prime: f_prime, real_range: real_range, imag_range: imag_range, iterations: iterations, tolerance: tolerance, depth: depth, prefix: "#{prefix}  ")
@@ -315,7 +315,7 @@ module MB
 
           x_gain = x - prev_x
           y_gain = y.abs - prev_y.abs
-          if x_gain.abs < tolerance || y_gain.abs < tolerance
+          if x_gain.abs < tolerance ** 2 || y_gain.abs < tolerance ** 2
             puts "#{prefix}\e[33mImprovement is very slow on loop #{l} (x_gain=#{x_gain} y_gain=#{y_gain})\e[0m" if $DEBUG
           elsif y_gain > 0
             puts "#{prefix}\e[33mY got worse on loop #{l} (x_gain=#{x_gain} y_gain=#{y_gain} x,y=#{x},#{y} prevx,prevy=#{prev_x},#{prev_y}\e[0m" if $DEBUG
@@ -324,6 +324,36 @@ module MB
           # Stop if we made no progress
           if x_gain == 0
             puts "#{prefix}  \e[31mNo progress made on loop #{last_loop} at depth #{depth}\e[0m" if $DEBUG
+
+            if depth < 0 # XXX
+              puts "trying rounding imag at depth #{depth}"
+              new_x = find_one_root(Complex(x.real, y.real.round(6)), f, depth: depth + 1)
+              new_y = f.call(new_x)
+
+              if new_y.abs >= y.abs
+                puts "trying rounding real at depth #{depth}"
+                new_x = find_one_root(Complex(x.real.round(6), y.real), f, depth: depth + 1)
+                new_y = f.call(new_x)
+              end
+
+              if new_y.abs >= y.abs
+                puts "trying rounding at depth #{depth}"
+                new_x = find_one_root(MB::M.round(x, 6), f, depth: depth + 1)
+                new_y = f.call(new_x)
+              end
+
+              if new_y.abs < y.abs
+                puts "somehow that worked at depth #{depth}"
+
+                # XXX require 'pry-byebug'; binding.pry if depth == 0 # XXX
+
+                x = new_x
+                y = new_y
+
+                next # XXX
+              end
+            end
+
             break
           end
 
